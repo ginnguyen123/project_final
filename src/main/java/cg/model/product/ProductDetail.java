8008f3e642f95585c2c23755f5f64405c7f7fb1e
sen <sen20312123@gmail.com>
2023-05-31 08:33:16 +0700

    no message

--- src/main/java/cg/model/product/ProductDetail.java
+++ src/main/java/cg/model/product/ProductDetail.java
@@ -1,43 +1,43 @@
-//package cg.model.product;
-//
-//import cg.model.BaseEntity;
-//import cg.model.enums.EColor;
-//import cg.model.enums.EProductStatus;
-//import cg.model.enums.ESize;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//import java.util.List;
-//
-//@Setter
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "product_details")
-//public class ProductDetail extends BaseEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String id;
-//
-//    private ESize size;
-//
-//    private EColor color;
-//
-//    private EProductStatus status;
-//
-//    @Column(name = "quantites", nullable = false)
-//    private Long quantity;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
-//    private Product product;
-//
-//    @OneToMany
-//    @JoinColumn(name = "media_id", referencedColumnName = "id")
-//    private List<ProductMedia> media;
-//}
+package cg.model.product;
+
+import cg.model.BaseEntity;
+import cg.model.enums.EColor;
+import cg.model.enums.EProductStatus;
+import cg.model.enums.ESize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_details")
+public class ProductDetail extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private ESize size;
+
+    private EColor color;
+
+    private EProductStatus status;
+
+    @Column(name = "quantites", nullable = false)
+    private Long quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    private Product product;
+
+    @OneToMany
+    @JoinColumn(name = "media_id", referencedColumnName = "id")
+    private List<ProductMedia> media;
+}
