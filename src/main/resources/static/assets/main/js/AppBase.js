285d60faf4d755f7894b85c4af0589804fb0d97e
sen <sen20312123@gmail.com>
2023-06-09 13:50:45 +0700

    no message

--- src/main/resources/static/assets/main/js/AppBase.js
+++ src/main/resources/static/assets/main/js/AppBase.js
@@ -72,15 +72,15 @@ class AppBase{
             cancelButtonText: 'Không xóa',
         })
     }
-    static showSuccessAlert(t) {
-        Swal.fire({
-            icon: 'success',
-            title: t,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 1500
-        })
-    }
+    // static showSuccessAlert(t) {
+    //     Swal.fire({
+    //         icon: 'success',
+    //         title: t,
+    //         position: 'top-end',
+    //         showConfirmButton: false,
+    //         timer: 1500
+    //     })
+    // }
     //
     // static showErrorAlert(t) {
     //     Swal.fire({
