a02484cc44286f64a6fb9db8eae90c9ba7265371
sen <sen20312123@gmail.com>
2023-06-28 16:54:04 +0700

    update cart service

--- src/main/java/cg/api/CartAPI.java
+++ src/main/java/cg/api/CartAPI.java
@@ -61,7 +61,7 @@ public class CartAPI {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updateCart(@PathVariable Long id, @Validated CartUpReqDTO cartUpReqDTO, BindingResult bindingResult ) throws IOException {
+    public ResponseEntity<?> updateCart(@PathVariable Long id, @Validated @RequestBody CartUpReqDTO cartUpReqDTO, BindingResult bindingResult ) throws IOException {
         new CartUpReqDTO().validate(cartUpReqDTO,bindingResult);
 
         cartUpReqDTO.setId(id);
