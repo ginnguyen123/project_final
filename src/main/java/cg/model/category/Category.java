b350833d06e7b481bc57925e0c497fa7fde3e0e3
ginnguyen123 <nhuunghia95@gmail.com>
2023-06-28 09:32:24 +0700

    ...

--- src/main/java/cg/model/category/Category.java
+++ src/main/java/cg/model/category/Category.java
@@ -10,10 +10,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.List;
-import java.util.Set;
 
 
 @Getter
