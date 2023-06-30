156850369547c204d70feb35ad59b292173eb880
ginnguyen123 <nhuunghia95@gmail.com>
2023-06-30 15:19:58 +0700

    fix query find product by discount time

--- src/main/java/cg/dto/product/client/ProductResClientDTO.java
+++ src/main/java/cg/dto/product/client/ProductResClientDTO.java
@@ -2,9 +2,15 @@ package cg.dto.product.client;
 
 import cg.dto.brand.BrandDTO;
 import cg.dto.category.client.CategoryClientDTO;
+import cg.model.brand.Brand;
+import cg.model.category.Category;
+import cg.model.discount.Discount;
+import cg.model.media.Media;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
@@ -19,4 +25,18 @@ public class ProductResClientDTO {
     private String avatar;
     private String brand;
     private CategoryClientDTO category;
+
+    public ProductResClientDTO(Long id, String title, String code, BigDecimal price, Discount discount, Media avatar, Brand brand, Category category){
+        Long pricePercent = price.multiply(BigDecimal.valueOf(discount.getDiscount())).divide(BigDecimal.valueOf(100)).longValue();
+        Long priceDiscount = price.subtract(BigDecimal.valueOf(pricePercent)).longValue();
+        this.id = id;
+        this.title = title;
+        this.code = code;
+        this.price = String.valueOf(price.longValue());
+        this.percent = discount.getDiscount();
+        this.priceDiscount = priceDiscount;
+        this.avatar = avatar.getFileUrl();
+        this.brand = brand.getName();
+        this.category = category.toCategoryClientDTO();
+    }
 }
