9a7bc15ff2c301cc8fe806e0c8995372c2243e61
sen <sen20312123@gmail.com>
2023-07-14 11:11:05 +0700

    productImport

--- src/main/java/cg/utils/ProductRequest.java
+++ src/main/java/cg/utils/ProductRequest.java
@@ -1,22 +1,26 @@
 package cg.utils;
 
+import cg.model.product.Product;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
+@Data
+@NoArgsConstructor
 public class ProductImportRequest {
-
-    private String keyword;
 
     private LocalDate fromDate;
 
     private LocalDate toDate;
 
-    private Long quantityMin;
+    private String keyword;
 
-    private Long quantityMax;
+    private String status;
 }
